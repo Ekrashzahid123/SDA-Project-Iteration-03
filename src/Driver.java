688018675013815ff7644d53382aaf162b57500c
Ekrash Zahid <mekrashzahid@gmail.com>
2025-01-02 22:56:32 +0500

    pushing driver updated class

--- src/Driver.java
+++ src/Driver.java
@@ -19,3 +19,4 @@ public class Driver extends User {
         System.out.println("Rating passenger with " + rating + " stars. Feedback: " + feedback);
     }
 }
+//done
--- src/Passanger.java
+++ src/Passanger.java
@@ -19,5 +19,6 @@ public class Passanger extends User {
         System.out.println("Rating driver with " + rating + " stars. Feedback: " + feedback);
     }
 }
+//done by Ekrash 
 
 
