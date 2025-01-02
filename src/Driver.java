7f1ac1e64cf6759455a2ecb664999aa217f9c318
Ekrash Zahid <mekrashzahid@gmail.com>
2025-01-02 22:44:18 +0500

    pushing passanger class

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,21 @@
-public class Driver {
-    
+public class Driver extends User {
+    public Driver(String id, String name, String email, String phone, String password) {
+        super(id, name, email, phone, password);
+    }
+
+    public void acceptRide(String rideId) {
+        System.out.println("Accepting ride: " + rideId);
+    }
+
+    public void rejectRide(String rideId) {
+        System.out.println("Rejecting ride: " + rideId);
+    }
+
+    public void updateLocation(String location) {
+        System.out.println("Updating location to: " + location);
+    }
+
+    public void ratePassenger(int rating, String feedback) {
+        System.out.println("Rating passenger with " + rating + " stars. Feedback: " + feedback);
+    }
 }
