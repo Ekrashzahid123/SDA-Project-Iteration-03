5a11f0d64fe0f325c614094b61facc71ae4d54b8
Ekrashzahid123 <147089512+Ekrashzahid123@users.noreply.github.com>
2025-01-15 00:21:52 +0500

    Update WheelchairPaymentStrategy.java

--- src/WheelchairPaymentStrategy.java
+++ src/WheelchairPaymentStrategy.java
@@ -2,10 +2,17 @@ public class WheelchairPaymentStrategy implements PaymentStrategyInterface {
 
     @Override
     public void processPayment(Payment payment, double amount) {
-        double discount = 0.05;  // Wheelchair users get a 5% discount
+        if ("Completed".equals(payment.getStatus())) {
+            System.out.println("Payment is already completed.");
+            return; 
+        }
+
+        double discount = 0.05;
         double discountedAmount = amount - (amount * discount);
-        payment.setAmount(discountedAmount);
-        payment.processPayment();
+        payment.setAmount(discountedAmount); 
+
+        payment.processPayment();  
+
         System.out.println("Wheelchair user payment of " + discountedAmount + " processed.");
     }
 }
